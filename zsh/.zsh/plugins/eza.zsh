f58ecbb1b3c9e47b30249bc007475fd09ceb3e4b
yaoyuan <yaoyuan@doinlab.com>
2025-05-16 16:59:53 +0800

    change eza alias l to ll

--- zsh/.zsh/plugins/eza.zsh
+++ zsh/.zsh/plugins/eza.zsh
@@ -1,6 +1,6 @@
 _EZA_PARAMS=('--git' '--group' '--group-directories-first' '--time-style=long-iso' '--color-scale=all' '--icons' '--header')
 
 alias ls='eza $_EZA_PARAMS'
-alias tree='eza --tree $eza_params'
-alias l='eza $_EZA_PARAMS -l'
+alias ll='eza $_EZA_PARAMS -l'
 alias lh='eza $_EZA_PARAMS -lha'
+alias tree='eza --tree $eza_params'
