9b7814eb93fa16443317426527d09a5e2a3be85f
yaoyuan <yaoyuan@doinlab.com>
2025-05-16 22:05:00 +0800

    replace eza alias lh to la

--- zsh/.zsh/plugins/eza.zsh
+++ zsh/.zsh/plugins/eza.zsh
@@ -2,5 +2,5 @@ _EZA_PARAMS=('--git' '--group' '--group-directories-first' '--time-style=long-is
 
 alias ls='eza $_EZA_PARAMS'
 alias ll='eza $_EZA_PARAMS -l'
-alias lh='eza $_EZA_PARAMS -lha'
+alias la='eza $_EZA_PARAMS -lha'
 alias tree='eza --tree $eza_params'
