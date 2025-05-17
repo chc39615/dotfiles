1c7db3667de4e1c93c39cdebf44467880bdc18e3
yaoyuan <yaoyuan@doinlab.com>
2025-05-17 15:50:48 +0800

    add tmux configuration

---
+++ scripts/stow_tmux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Fix the path to the dotfiles folder
+DOTFILES_DIR="$HOME/dotfiles"
+
+# Directory for tmux dotfiles
+SOURCE_DIR="$DOTFILES_DIR/tmux"
+
+# Target directory (home directory)
+TARGET_DIR="$HOME"
+
+# Source the utils script
+source "$DOTFILES_DIR/scripts/utils.sh"
+
+# run the stow function for the bash dotfiles
+stow_dotfiles "$SOURCE_DIR" "$TARGET_DIR"
