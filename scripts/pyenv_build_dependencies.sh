28aad92d2d22dfa4a25b26680dff6c27adcf36f7
yaoyuan <yaoyuan@doinlab.com>
2025-05-29 18:21:41 +0800

    add pyenv build dependencies for ubuntu

---
+++ scripts/pyenv_build_dependencies.sh
@@ -0,0 +1,26 @@
+# pyenv build python dependencies for ubuntu
+sudo apt update
+sudo apt install -y \
+  build-essential \
+  libbz2-dev \
+  libncurses5-dev \
+  libncursesw5-dev \
+  libreadline-dev \
+  libssl-dev \
+  libsqlite3-dev \
+  libffi-dev \
+  liblzma-dev \
+  zlib1g-dev \
+  tk-dev \
+  uuid-dev \
+  libnss3-dev \
+  libgdbm-dev \
+  libdb-dev \
+  libexpat1-dev \
+  libx11-dev \
+  libxt-dev \
+  wget \
+  curl \
+  llvm \
+  make
+
