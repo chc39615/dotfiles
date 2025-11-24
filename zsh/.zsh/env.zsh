eda538986fe4ab3f4e1fdebf77e4389a428204f5
yaoyuan <yaoyuan@doinlab.com>
2025-11-24 22:23:53 +0800

    set bat as MANPAGER

--- zsh/.zsh/env.zsh
+++ zsh/.zsh/env.zsh
@@ -11,3 +11,8 @@ export LC_ALL="en_US.UTF-8"
 # add "$HOME/.local/bin/" to PATH
 . "$HOME/.local/bin/env"
 
+# Use bat as man pager only if it's installed
+command -v bat >/dev/null && export MANPAGER="sh -c 'col -b | bat -l man -p'"
+# man outputs the page with ^H overstrike formatting
+# col -b converts ^H sequences → proper ANSI escape codes (or removes them if you prefer plain)
+# bat -l man -p receives clean input, applies nice syntax highlighting, and displays nothing but the actual text
